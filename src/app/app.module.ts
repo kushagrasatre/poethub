322ba7c89e3a90f660e2763b4ff2e52a1a8358d2
kushagra <kushagrasatre@gmail.com>
2019-03-24 22:17:57 +0530

    signup code wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DatadisplayComponent } from './datadisplay/datadisplay.component';
@@ -35,7 +35,9 @@ import { RegistrationComponent } from './registration/registration.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+    
   ],
   providers: [ServicedataService],
   bootstrap: [AppComponent]
