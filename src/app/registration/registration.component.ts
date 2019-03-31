d29b68a7bff833461b14d2ad2cb87bebbba186b4
kushagra <kushagrasatre@gmail.com>
2019-03-31 14:49:23 +0530

    registration form relaited changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup, FormBuilder} from "@angular/forms";
+import { FormControl,FormGroup, FormBuilder, Validators} from "@angular/forms";
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -11,14 +11,14 @@ export class RegistrationComponent implements OnInit {
   ngOnInit() {
   }
   profileForm = this.fb.group({
-      fullname: [''],
-      City: [''],
-      State: [''],
-      email: [''],
-      date: [''],
-      Zip: [''],
-      password: [''],
-      gender: ['']
+      fullname: ['',Validators.required],
+      City: ['',Validators.required],
+      State: ['',Validators.required],
+      email: ['',Validators.required, Validators.email],
+      date: ['',Validators.required],
+      Zip: ['',Validators.required],
+      password: ['',Validators.required],
+      gender: ['',Validators.required]
     });
     updateProfile(){
     console.warn(this.profileForm.value);
